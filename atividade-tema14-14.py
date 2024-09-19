24877671be13b547c6bcd27045249718bbfa3d3c
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-19 01:18:11 -0300

    Add files via upload

---
+++ atividade-tema14-14.py
@@ -0,0 +1,11 @@
+# Programa para calcular a área de um retângulo
+
+# Leitura das dimensões do retângulo
+base = float(input("Digite o valor da base do retângulo: "))
+altura = float(input("Digite o valor da altura do retângulo: "))
+
+# Cálculo da área
+area = base * altura
+
+# Exibição do resultado
+print("A área do retângulo é: ", area)
